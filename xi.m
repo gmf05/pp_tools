d79fff9bfa94b792e7a9addf40706b5384941425
Grant <gfiddyment@gmail.com>
2014-02-27 10:15:12 -0500

    Added check to prevent division by 0 with bad models

--- xi.m
+++ xi.m
@@ -17,7 +17,7 @@ function XI = xi(ms,d,lag)
         numer = sum(d.dn(i,:).*d.dn(j,:));
         denom = sum(cifs(i,:).*cifs(j,:));
         if denom>0
-          XI(i,j) = sum(d.dn(i,:).*d.dn(j,:)) / sum(cifs(i,:).*cifs(j,:));
+          XI(i,j) = numer / denom;  
         end
       end
     end
@@ -26,7 +26,11 @@ function XI = xi(ms,d,lag)
   else
     for i = 1:d.N_channels
       for j = 1:d.N_channels
-        XI(i,j) = sum(d.dn(i,lag+1:end).*d.dn(j,1:end-lag)) / sum(cifs(i,lag+1:end).*cifs(j,1:end-lag));
+        numer = sum(d.dn(i,lag+1:end).*d.dn(j,1:end-lag));
+        denom = sum(cifs(i,lag+1:end).*cifs(j,1:end-lag));
+        if denom>0
+        	XI(i,j) = numer / denom; 
+        end
       end
     end
   end
