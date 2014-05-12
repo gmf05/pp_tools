f8753a488426c9ee0eac10f5eb16b560aabbf68d
grantf <gfiddyment@gmail.com>
2014-05-12 17:15:34 -0400

    Simple starter script

--- MGH/compare_spike_amp.m
+++ MGH/compare_spike_amp.m
@@ -1,1 +1,13 @@
-%
+% load /projectnb/ecog/Data/MG49/MG49_Seizure45_LFP_filtered
+
+figure
+thresh = 4; spk_style = 'ro';
+
+% N_channels = size(d_post,2);
+% for n = 1:N_channels
+for n = 30
+  spkind = hilbertspike(d_post(:,n),thresh,1);
+  plot(time,d_post(:,n),'b'); hold on;
+  plot(time(spkind),d_post(spkind,n),spk_style);
+%   pause; clf;
+end
