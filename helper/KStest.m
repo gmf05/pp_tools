f676f30173cd8a6acfb0b28822c03b6274f5b1d6
Grant <gfiddyment@gmail.com>
2014-06-09 08:50:22 -0400

    Auxillary function to compute KS statistic

---
+++ helper/KStest.m
@@ -0,0 +1,28 @@
+function [ks_stat, ks_ci, z] = ks_test(y,cif)
+
+spike_ind = find(y);           
+numISIs = length(spike_ind)-1;
+
+if numISIs<3
+  error('Too few data points');
+end
+
+z = zeros(1, numISIs);
+for j=1:numISIs                                                
+  z(j) = sum(cif(spike_ind(j)+1:spike_ind(j+1)));
+end
+
+%---- rescale function is expontential               
+rs_fn = @(x)(1-exp(-x));
+rs_cdf = @(x)(unifcdf(x,0,1));
+% % %---- rescale function is identity
+% % rs_fn = @(x)(x);
+% % rs_cdf = @(x)(expcdf(x,1));
+
+z = rs_fn(z);
+[eCDF,xCDF] = ecdf(sort(z));
+aCDF = rs_cdf(xCDF);
+ks_stat = max(abs(aCDF-eCDF));
+ks_ci = 1.96/sqrt(numISIs+1);
+
+end
