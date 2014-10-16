b8b3125a58cb84605773020b03aba81f293a7f02
grantf <gfiddyment@gmail.com>
2014-10-16 16:00:35 -0400

    Tweaked spike-finder. Seems to work well now

--- helper/spikefind.m
+++ helper/spikefind.m
@@ -17,16 +17,19 @@ function spikes = spikefind(x,thresh,dT)
 T = length(x);
 spikes=[];
 
-for t = dT+1:dT:T-dT
+t=dT+1;
+while t<=T-dT
   t1 = t-dT;
   t2 = t+dT;
   dx1=x(t)-x(t1);
   dx2=x(t2)-x(t);
-  dx1*dx2
   if dx1*dx2<thresh && dx1<dx2    
     [~,ti] = min(x(t1:t2)); % find local min on [t1,t2]
     t0 = t1-1+ti; % index shifted from [t1,t2] to [1,T]
     spikes=[spikes t0];
+    t=t2+dT;
+  else
+    t=t+1;
   end
 end
 
